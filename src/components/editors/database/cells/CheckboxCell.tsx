4ff4eba771e6f1d4c119d53796380c4e0fc1f19a
Felix Lee <felixlee175@gmail.com>
2026-06-04 23:55:00 +0700

    updated note and database nestling checkbox colors

--- src/components/editors/database/cells/CheckboxCell.tsx
+++ src/components/editors/database/cells/CheckboxCell.tsx
@@ -16,9 +16,12 @@ export default function CheckboxCell({
         checked={checked}
         onCheckedChange={(checked) => onSave(checked.toString())}
         className={cn(
-          "size-5 border-2 data-[state=checked]:text-white",
-          "border-teal-500 data-[state=checked]:border-teal-400 data-[state=checked]:bg-teal-400",
-          "dark:border-teal-400 dark:data-[state=checked]:border-teal-500 dark:data-[state=checked]:bg-teal-500",
+          "size-5 border transition-colors",
+          "border-zinc-300 hover:border-zinc-400",
+          "dark:border-zinc-600 dark:hover:border-zinc-500",
+          "data-[state=checked]:border-teal-500 data-[state=checked]:bg-teal-500 data-[state=checked]:text-white",
+          "dark:data-[state=checked]:border-teal-400 dark:data-[state=checked]:bg-teal-400",
+          "focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-400",
         )}
       />
     </div>
