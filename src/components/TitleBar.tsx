adb6de3959915de48bb4e7c8bfd50aca0fb79fd5
Felix Lee <felixlee175@gmail.com>
2026-09-03 20:44:06 +0700

    added nest title, nest switch popover, and go to dashboard button in titlebar

--- src/components/TitleBar.tsx
+++ src/components/TitleBar.tsx
@@ -1,30 +1,87 @@
 import { cn } from "@/lib/utils/general";
-import { useActiveBackgroundId } from "@/stores/useNestStore";
+import {
+  useActiveBackgroundId,
+  useActiveNestId,
+  useNestActions,
+  useNests,
+} from "@/stores/useNestStore";
 import { getCurrentWindow } from "@tauri-apps/api/window";
 import {
+  ArrowLeft,
+  Menu,
   Minus,
   PanelLeftOpen,
   PanelRightOpen,
   Settings,
   X,
 } from "lucide-react";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import IconButton from "./ui/icon-button";
 import { useSettingsModal } from "@/stores/useModalStore";
 import {
   useSettingsActions,
   useSidebarHidden,
 } from "@/stores/useSettingsStore";
+import BasePopover from "./popovers/BasePopover";
+import NestSwitchPopover from "./popovers/NestSwitchPopover";
+import { useInlineEdit } from "@/hooks/useInlineEdit";
+import { clearLastNestId } from "@/lib/storage/nest";
+import { useNestlingActions } from "@/stores/useNestlingStore";
+import { useNavigate } from "react-router-dom";
 
 export default function Titlebar() {
   const appWindow = getCurrentWindow();
-  const activeBackgroundId = useActiveBackgroundId();
 
+  const nestId = useActiveNestId();
+  const nest = useNests().find((n) => n.id === nestId);
+  const navigate = useNavigate();
+
+  const activeBackgroundId = useActiveBackgroundId();
+  const { setActiveNestlingId } = useNestlingActions();
+  const { setActiveBackgroundId, setActiveNestId, updateNest } =
+    useNestActions();
   const sidebarHidden = useSidebarHidden();
   const { setIsSettingsOpen } = useSettingsModal();
   const { setSetting } = useSettingsActions();
 
+  const handleExit = () => {
+    navigate("/dashboard");
+    clearLastNestId();
+    setActiveBackgroundId(null);
+    setActiveNestlingId(null);
+    setActiveNestId(null);
+  };
+
+  const [isOpen, setIsOpen] = useState(false);
   const [isMaximized, setIsMaximized] = useState(false);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleDoubleClick = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    setIsEditing(true);
+  };
+
+  const {
+    value: title,
+    setValue: setTitle,
+    isEditing,
+    setIsEditing,
+    handleBlur,
+    handleKeyDown,
+  } = useInlineEdit({
+    initialValue: nest?.title ?? "",
+    onSave: (title) => {
+      if (nestId) updateNest(nestId, title);
+    },
+  });
+
+  useEffect(() => {
+    if (isEditing && inputRef.current) {
+      inputRef.current.focus();
+      inputRef.current.select();
+    }
+  }, [isEditing]);
 
   useEffect(() => {
     const updateMaximized = async () => {
@@ -47,18 +104,70 @@ export default function Titlebar() {
       data-tauri-drag-region
       className="fixed z-50 flex h-8 w-full items-center justify-between dark:text-white"
     >
-      <div
-        className={cn(
-          "flex h-full items-center justify-center rounded-br-md",
-          activeBackgroundId && "bg-white/50 backdrop-blur-sm dark:bg-black/50",
-        )}
-      >
-        <IconButton
-          label="Toggle Sidebar"
-          Icon={sidebarHidden ? PanelLeftOpen : PanelRightOpen}
-          onClick={() => setSetting("sidebarHidden", !sidebarHidden)}
-        />
-      </div>
+      {nestId ? (
+        <div
+          className={cn(
+            "flex h-full items-center justify-center rounded-br-md",
+            activeBackgroundId &&
+              "bg-white/50 backdrop-blur-sm dark:bg-black/50",
+          )}
+        >
+          <IconButton label="Go Back" Icon={ArrowLeft} onClick={handleExit} />
+
+          <IconButton
+            label="Toggle Sidebar"
+            Icon={sidebarHidden ? PanelLeftOpen : PanelRightOpen}
+            onClick={() => setSetting("sidebarHidden", !sidebarHidden)}
+          />
+
+          <BasePopover
+            isOpen={isOpen}
+            setIsOpen={setIsOpen}
+            align="start"
+            side="bottom"
+            width="w-72"
+            trigger={
+              <div>
+                <IconButton
+                  label="Switch Nest"
+                  Icon={Menu}
+                  onClick={() => setIsOpen(!isOpen)}
+                />
+              </div>
+            }
+            content={<NestSwitchPopover onClose={() => setIsOpen(false)} />}
+          />
+
+          <div
+            className={cn(
+              "ml-1 cursor-text rounded-lg text-zinc-900 transition-all dark:text-zinc-100",
+              isEditing
+                ? "px-3 shadow-md ring ring-teal-500"
+                : "hover:opacity-80",
+            )}
+            onDoubleClick={handleDoubleClick}
+          >
+            <input
+              ref={inputRef}
+              id="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              onBlur={handleBlur}
+              onKeyDown={handleKeyDown}
+              readOnly={!isEditing}
+              autoComplete="off"
+              autoCorrect="off"
+              spellCheck={false}
+              className={cn(
+                "truncate bg-transparent text-xs font-semibold tracking-tight focus:outline-none",
+                !isEditing && "pointer-events-none",
+              )}
+            />
+          </div>
+        </div>
+      ) : (
+        <div />
+      )}
 
       <div
         className={cn(
@@ -66,11 +175,13 @@ export default function Titlebar() {
           activeBackgroundId && "bg-white/50 backdrop-blur-sm dark:bg-black/50",
         )}
       >
-        <IconButton
-          label="Settings"
-          Icon={Settings}
-          onClick={() => setIsSettingsOpen(true)}
-        />
+        {nestId && (
+          <IconButton
+            label="Settings"
+            Icon={Settings}
+            onClick={() => setIsSettingsOpen(true)}
+          />
+        )}
 
         <div
           onClick={() => appWindow.minimize()}
